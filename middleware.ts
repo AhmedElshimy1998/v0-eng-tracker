c58c840712998b90252fcf8c64f67844f42de0fa
Ahmed Elshimy <35340629+AhmedElshimy1998@users.noreply.github.com>
2026-03-15 17:47:00 +0200

    api

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse, NextRequest } from "next/server"; // أضفنا NextRequest هنا
 
 // تحديد المسارات العامة
-const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)']);
+const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)', '/api(.*)', '/api']);
 
 export default clerkMiddleware(async (auth, req: NextRequest) => { // حددنا النوع هنا req: NextRequest
   const { userId } = await auth();
