0c5752b1393f6df3a121f1c30b641dd1d0e80619
Ahmed Elshimy <35340629+AhmedElshimy1998@users.noreply.github.com>
2026-03-17 02:41:16 +0200

    tracker

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -6,7 +6,6 @@ import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
-import { GraduationCap } from "lucide-react"; // استيراد أيقونة مناسبة للترمات
 
 
 const navigation = [
