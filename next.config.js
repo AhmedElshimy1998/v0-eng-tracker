fac7ae9a1904f4108e586691484d590e796b293e
Ahmed Elshimy <35340629+AhmedElshimy1998@users.noreply.github.com>
2026-03-26 01:49:30 +0200

    app edite

---
+++ next.config.js
@@ -0,0 +1,19 @@
+const withPWAInit = require("@ducanh2912/next-pwa").default;
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  turbopack: {}, // لحل مشكلة محرك البناء اللي ظهرتلك المرة اللي فاتت
+};
+
+module.exports = withPWA(nextConfig);
