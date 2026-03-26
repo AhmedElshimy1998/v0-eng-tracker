7ce596d754b5947656533c7d3565efaa34e5f569
Ahmed Elshimy <35340629+AhmedElshimy1998@users.noreply.github.com>
2026-03-26 03:03:14 +0200

    sw

--- next.config.js
+++ next.config.js
@@ -3,37 +3,25 @@ const withPWAInit = require("@ducanh2912/next-pwa").default;
 const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
-  // توجيه صريح للمكتبة بمكان ملف الـ Worker الخاص بك
   customWorkerDir: "worker", 
-  // إجبار توليد ملف sw.js حتى في البيئات المعقدة
   sw: "sw.js",
   register: true,
-  setupExitSignals: true,
   skipWaiting: true,
-  // إضافة مهمة لضمان عمل الكاش أوفلاين
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
-  workboxOptions: {
-    disableDevLogs: true,
-    // هذا السطر يضمن دمج كود الإشعارات الخاص بك مع كود المكتبة
-    importScripts: [], 
-  },
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // مهم جداً لأن أي خطأ بسيط في التايب سكريبت بيوقف بناء الـ PWA
     ignoreBuildErrors: true, 
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
   images: {
     unoptimized: true,
   },
-  // تأكد من حذف سطر turbopack: {} نهائياً من هنا
+  // السطر ده هو اللي هيسكت الإيرور اللي ظهرلك
+  turbopack: {}, 
 };
 
 module.exports = withPWA(nextConfig);
