de7433b5b61c2eadc83edda8502bc0ceb8893935
Ahmed Elshimy <35340629+AhmedElshimy1998@users.noreply.github.com>
2026-03-26 12:25:40 +0200

    noti card

--- hooks/use-floating-prompts.tsx
+++ hooks/use-floating-prompts.tsx
@@ -3,7 +3,7 @@
 import React, { createContext, useContext, useState, useCallback } from 'react'
 
 interface PromptContextType {
-  activePrompts: string[]
+  activePrompts: string []
   register: (id: string) => void
   unregister: (id: string) => void
 }
